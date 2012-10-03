9e042fd2c151174921981773454d14819cbcced0
Matthew Ricks <matthew@sixricks.com>
2012-10-03 13:30:08 -0600

    HyPeerWebTest

---
+++ HyPeerWeb/src/dbPhase/hypeerweb/HyPeerWebTest.java
@@ -0,0 +1,114 @@
+package dbPhase.hypeerweb;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class HyPeerWebTest {
+    
+    HyPeerWeb hypeerweb;
+
+    @Before
+    public void setUp() {
+        try{
+            hypeerweb = HyPeerWeb.getSingleton();
+        }
+        catch(Exception e){
+            hypeerweb = null;
+        }
+    }
+
+    @Test
+    public void testHyPeerWeb() {
+        assertTrue(hypeerweb.size() == 0);
+    }
+
+    @Test
+    public void testGetSingleton() {
+        assertTrue(hypeerweb != null);
+    }
+
+    @Test
+    public void testClear() {
+        hypeerweb.clear();
+        assertTrue(hypeerweb.size() == 0);
+        hypeerweb.addNode(new Node(0));
+        hypeerweb.clear();
+        assertTrue(hypeerweb.size() == 0);
+    }
+
+    @Test
+    public void testSize() {
+        assertTrue(hypeerweb.size() == 0);
+        hypeerweb.addNode(new Node(0));
+        assertTrue(hypeerweb.size() == 1);
+    }
+
+    @Test
+    public void testReloadString() {
+        hypeerweb.saveToDatabase();
+        hypeerweb.addNode(new Node(0));
+        hypeerweb.reload(HyPeerWebDatabase.DEFAULT_DATABASE_NAME);
+        assertTrue(hypeerweb.size() == 0);
+    }
+
+    @Test
+    public void testGetNode() {
+        Node node0 = new Node(0);
+        hypeerweb.addNode(node0);
+        assertTrue(hypeerweb.getNode(0) == node0);
+        Node node1 = new Node(1);
+        hypeerweb.addNode(node1);
+        assertTrue(hypeerweb.getNode(1) == node1);
+        assertTrue(hypeerweb.getNode(0) == node0);
+    }
+
+    @Test
+    public void testReload() {
+        hypeerweb.saveToDatabase();
+        hypeerweb.addNode(new Node(0));
+        hypeerweb.reload();
+        assertTrue(hypeerweb.size() == 0);
+    }
+
+    @Test
+    public void testSaveToDatabase() {
+        hypeerweb.saveToDatabase();
+        hypeerweb.addNode(new Node(0));
+        hypeerweb.reload();
+        assertTrue(hypeerweb.size() == 0);
+    }
+
+    @Test
+    public void testAddNode() {
+        Node node0 = new Node(0);
+        hypeerweb.addNode(node0);
+        assertTrue(hypeerweb.getNode(0) == node0);
+        Node node1 = new Node(1);
+        hypeerweb.addNode(node1);
+        assertTrue(hypeerweb.getNode(1) == node1);
+    }
+
+    @Test
+    public void testContains() {
+        Node node0 = new Node(0);
+        hypeerweb.addNode(node0);
+        assertTrue(hypeerweb.contains(node0));
+        Node node1 = new Node(1);
+        hypeerweb.addNode(node1);
+        assertTrue(hypeerweb.contains(node1));
+    }
+
+    @Test
+    public void testAddToHyPeerWeb() {
+        Node node0 = new Node(0);
+        hypeerweb.addNode(node0);
+        Node node1 = new Node(0);
+        hypeerweb.addToHyPeerWeb(node1, node0);
+        assertTrue(hypeerweb.size() == 2);
+        assertTrue(hypeerweb.getNode(0) == node0);
+        assertTrue(hypeerweb.getNode(1) == node1);
+    }
+
+}
