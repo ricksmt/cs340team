c6fec8e8844b41e4d858dde992f66762a5cea8ba
Brian <herobd@gmail.com>
2012-10-03 11:39:01 -0600

    Fleshing out of NodeTest

--- HyPeerWeb/src/dbPhase/testing/NodeTest.java
+++ HyPeerWeb/src/dbPhase/testing/NodeTest.java
@@ -3,6 +3,12 @@ package dbPhase.testing;
 import junit.framework.TestCase;
 import dbPhase.hypeerweb.Node;
 
+/**
+ * 
+ * @author Brian Daviss
+ *
+ */
+
 public class NodeTest extends TestCase
 {
    //specifically tests that it is being inserted at the right spot. The connections class is assumed to be working.
@@ -44,5 +50,31 @@ public class NodeTest extends TestCase
         Node newNode9 = new Node(-1);
         newNode9.insertSelf(firstNode);
         assert newNode9.getWebId() == 9;
+    }
+    
+    public void testGetLowestNeighborWithoutChild()
+    {
+        Node node0 = new Node(0);
+        assert node0.getLowestNeighborWithoutChild() == node0;
+        
+        Node node1 = new Node(1);
+        //node1.setFold(node0);
+        //node0.setFold(node1);
+        node1.addNeighbor(node0);
+        node0.addNeighbor(node1);
+        assert node1.getLowestNeighborWithoutChild() == node0;
+        assert node0.getLowestNeighborWithoutChild() == node0;
+        
+        Node node2 = new Node(2);
+        Node node3 = new Node(3);
+        node0.addNeighbor(node2);
+        node1.addNeighbor(node3);
+        node2.addNeighbor(node0);
+        node2.addNeighbor(node3);
+        node3.addNeighbor(node2);
+        node3.addNeighbor(node1);
+        
+        assert node3.getLowestNeighborWithoutChild() == node1;
+        assert node2.getLowestNeighborWithoutChild() == node0; 
     }
 }
