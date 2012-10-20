c4f63f546031fbddf4a8ec53458e47dd8304b957
Matthew Ricks <matthew@sixricks.com>
2012-10-20 13:32:51 -0600

    Added black package

---
+++ HyPeerWeb/src/black/HyPeerWebBlack.java
@@ -0,0 +1,94 @@
+package black;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class HyPeerWebBlack {
+
+    @BeforeClass
+    public static void setUpBeforeClass() throws Exception {
+    }
+
+    @AfterClass
+    public static void tearDownAfterClass() throws Exception {
+    }
+
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @After
+    public void tearDown() throws Exception {
+    }
+
+    @Test
+    public void testHyPeerWeb() {
+        fail("Not yet implemented"); // TODO
+    }
+
+    @Test
+    public void testGetSingleton() {
+        fail("Not yet implemented"); // TODO
+    }
+
+    @Test
+    public void testClear() {
+        fail("Not yet implemented"); // TODO
+    }
+
+    @Test
+    public void testSize() {
+        fail("Not yet implemented"); // TODO
+    }
+
+    @Test
+    public void testReloadString() {
+        fail("Not yet implemented"); // TODO
+    }
+
+    @Test
+    public void testGetNode() {
+        fail("Not yet implemented"); // TODO
+    }
+
+    @Test
+    public void testReload() {
+        fail("Not yet implemented"); // TODO
+    }
+
+    @Test
+    public void testSaveToDatabase() {
+        fail("Not yet implemented"); // TODO
+    }
+
+    @Test
+    public void testAddNode() {
+        fail("Not yet implemented"); // TODO
+    }
+
+    @Test
+    public void testContains() {
+        fail("Not yet implemented"); // TODO
+    }
+
+    @Test
+    public void testAddToHyPeerWeb() {
+        fail("Not yet implemented"); // TODO
+    }
+
+    @Test
+    public void testRemoveFromHyPeerWeb() {
+        fail("Not yet implemented"); // TODO
+    }
+
+    @Test
+    public void testRemoveFromHyPeerWebInt() {
+        fail("Not yet implemented"); // TODO
+    }
+
+}
