42d8828884bfbfab81b35b064d01c9b80cd7adff
Konrad <Konrad@Konrad-PC>
2012-10-29 13:48:47 -0600

    SendVisitor Implemented

--- HyPeerWeb/src/hypeerweb/SendVisitor.java
+++ HyPeerWeb/src/hypeerweb/SendVisitor.java
@@ -1,5 +1,7 @@
 package hypeerweb;
 
+import java.util.Set;
+
 /**
  * Used to send a message from a source node to a target node.
  * The actual message is the targetOperation to be performed on the target Node.
@@ -23,6 +25,7 @@ public class SendVisitor implements Visitor
      */
     public SendVisitor()
     { 
+        TARGET_KEY = "target";
     }
     
     /**
@@ -61,7 +64,17 @@ public class SendVisitor implements Visitor
      * @post True
      */
     protected void targetOperation(Node node, Parameters parameters){
+        Contents contents = node.getContents();
         
+        Set<String> keySet = parameters.getKeys();
+        
+        for(String key : keySet){
+            if (contents.containsKey(key))
+            {
+                    System.err.println("Node " + node.getWebId() + " has already been visited ");
+            }
+            contents.set(key, parameters.get(key));
+        }
     }
     
     /**
@@ -78,5 +91,80 @@ public class SendVisitor implements Visitor
     protected void intermediateOperation(final Node node, final Parameters parameters)
     {
         assert node != null && parameters != null && node != Node.NULL_NODE;
+        
+        Contents contents = node.getContents();
+        
+        Set<String> keySet = parameters.getKeys();
+        
+        for(String key : keySet){
+            
+            contents.set(key, parameters.get(key));
+        }
+
+        
+        Node nextIntermediateNode = getIntermediateNode(node, (Integer) parameters.get(TARGET_KEY));
+        nextIntermediateNode.accept(this, parameters);
+        
     }
+
+    private Node getIntermediateNode(Node node, int targetId) {
+        Set<Node> nodeSet = node.connections.getNeighbors();
+        int distance = SimplifiedNodeDomain.distanceTo(node.getWebId(), targetId);
+        Node closestNode = Node.NULL_NODE;
+        int closestDistance=distance;
+        
+        for(Node neighbor : nodeSet){
+            int curDistance=SimplifiedNodeDomain.distanceTo(neighbor.getWebId(), targetId);
+            if(curDistance<closestDistance){
+                closestNode=neighbor;
+                closestDistance=curDistance;
+                break;
+            }
+        }        
+        nodeSet=node.connections.getSurrogateNeighbors();
+        for(Node surNeighbor : nodeSet){
+            int curDistance=SimplifiedNodeDomain.distanceTo(surNeighbor.getWebId(), targetId);
+            
+            if(curDistance<closestDistance){
+                closestNode=surNeighbor;
+                closestDistance=curDistance;
+            }
+        }        
+        nodeSet=node.connections.getInverseSurrogateNeighbors();
+        for(Node invSurNeighbor : nodeSet){
+            int curDistance=SimplifiedNodeDomain.distanceTo(invSurNeighbor.getWebId(), targetId);
+            
+            if(curDistance<closestDistance){
+                closestNode=invSurNeighbor;
+                closestDistance=curDistance;
+            }
+        }        
+        Node fold=node.connections.getFold();
+        if(fold != Node.NULL_NODE){
+            int curDistance=SimplifiedNodeDomain.distanceTo(fold.getWebId(), targetId);
+            if(curDistance<closestDistance){
+                closestNode=fold;
+                closestDistance=curDistance;
+            }
+        }        
+        Node surFold=node.connections.getSurrogateFold();
+        if(surFold != Node.NULL_NODE){
+            int curDistance=SimplifiedNodeDomain.distanceTo(surFold.getWebId(), targetId);
+            if(curDistance<closestDistance){
+                closestNode=surFold;
+                closestDistance=curDistance;
+            }
+        }        
+        Node invSurFold=node.connections.getInverseSurrogateFold();
+        if(invSurFold != Node.NULL_NODE){
+            int curDistance=SimplifiedNodeDomain.distanceTo(invSurFold.getWebId(), targetId);
+            if(curDistance<closestDistance){
+                closestNode=invSurFold;
+                closestDistance=curDistance;
+            }
+        }        
+        return closestNode;        
+    }
+    
+    
 }
