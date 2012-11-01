e10db6f976836da73697093d0e4f433d3778c886
Matthew Ricks <matthew@sixricks.com>
2012-11-01 13:59:29 -0600

    Cleaned visitor abstractions. Please do not touch them.

--- HyPeerWeb/src/hypeerweb/BroadcastVisitor.java
+++ HyPeerWeb/src/hypeerweb/BroadcastVisitor.java
@@ -1,8 +1,5 @@
 package hypeerweb;
 
-import java.util.Set;
-import java.util.TreeSet;
-
 /**
  * Broadcasts a message from a source node to all nodes in the HyPeerWeb.
  * The message is actually the method operation(Node, Parameters) to be performed on all nodes.
@@ -12,14 +9,10 @@ import java.util.TreeSet;
  * 
  * @author Matthew
  */
-public class BroadcastVisitor implements Visitor
+public abstract class BroadcastVisitor implements Visitor
 {
     /** The key used to identify a key-value pair in the parameters list. */
     protected static String STARTED_KEY;
-    
-    /** The flag that tells whether the broadcast should be started from node zero
-     *  or continued from the current node*/
-    private boolean startFromZero;
 
     /**
      * The default constructor
@@ -29,7 +22,6 @@ public class BroadcastVisitor implements Visitor
      */
     public BroadcastVisitor()
     {
-        startFromZero=true;
     }
     
     /**
@@ -51,51 +43,7 @@ public class BroadcastVisitor implements Visitor
     {
         operation(node, parameters);
     }
-    /**
-     * broadcastFromZero
-     * @param node
-     * @param parameters
-     */
-    private void broadcastFromZero(Node node, Parameters parameters)
-    {
-        Node cap = node.findCapNode(node);
-        Node zero = cap.getFold();
-        if(zero.getWebId()!=0) zero = zero.getLowestNeighbor();
-        
-        zero.accept(this, parameters);
-    }
-    /**
-     * 
-     * 
-     * @return
-     */
-    private Set<Node> getNeighborsToBroadcast(Node node)
-    {
-        int nodeId = node.getWebId();
-        int height = node.getHeight();
-        Set<Node> neighborsToBroadcast = new TreeSet<Node>();
-        Set<Node> neighbors = node.connections.getNeighbors();
-        
-        double limit = Math.pow(2, height);
-        
-        for(int bit=1; bit<limit; bit*=2){
-            int idToBroadcast = nodeId | bit;
-            
-            if(idToBroadcast == nodeId) break; //reached the first '1'
-            else{
-                for(Node neighbor: neighbors){
-                    if(neighbor.getWebId()==idToBroadcast){
-                        neighborsToBroadcast.add(neighbor);
-                        break;
-                    }
-                }
-            }
-        }
-        
-        return neighborsToBroadcast;
-        
-        
-    }
+    
     /**
      * The abstract operation to be performed on all nodes.
      * This operation must be implemented in all concrete subclasses.
@@ -105,33 +53,5 @@ public class BroadcastVisitor implements Visitor
      * @pre node is not null and parameters is not null.
      * @post True
      */
-    private void operation(Node node, Parameters parameters){
-        if (node.getWebId() == 0)
-            startFromZero = false;
-
-        if (startFromZero)
-            broadcastFromZero(node, parameters);
-        else
-        {
-            Contents contents = node.getContents();
-            
-            Set<String> keySet = parameters.getKeys();
-            
-            for(String key : keySet){
-                if (contents.containsKey(key))
-                {
-                        System.err.println("Node " + node.getWebId() + " has already been visited ");
-                }
-                contents.set(key, parameters.get(key));
-            }
-
-            
-            
-            
-            for(Node nodeToBroadcast: getNeighborsToBroadcast(node)){
-                nodeToBroadcast.accept(this, parameters);
-            }
-        }
-
-    }
+    protected abstract void operation(Node node, Parameters parameters);
 }
