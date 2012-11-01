26ed057e697c10bc7b1c906eede134d20a3648da
Huy Pham <phq7621y@yahoo.com.vn>
2012-10-31 21:20:40 -0600

    node 2 update

--- HyPeerWeb/src/black/Node2.java
+++ HyPeerWeb/src/black/Node2.java
@@ -1,9 +1,14 @@
 package black;
 
 import hypeerweb.Node;
+import hypeerweb.SimplifiedNodeDomain;
+import hypeerweb.Node.State;
+
 import java.util.HashSet;
 import org.junit.After;
 import org.junit.Before;
+
+import testing.ExpectedResult;
 
 
 import junit.framework.TestCase;
@@ -28,45 +33,56 @@ public class Node2 extends TestCase
         node1 = new Node(0);
     }
     
-    public void testAddNeighbor() {
+    public void testAddNeighbor() 
+    {
         node0.addNeighbor(node1);
         HashSet<Integer> a = node0.getNeighborsIds();
         assert(!a.isEmpty());
     }
     
-    public void testRemoveNeighbor() {
+    public void testRemoveNeighbor() 
+    {
+        node0.addNeighbor(node1);
         node0.removeNeighbor(node1);
         HashSet<Integer> a = node0.getNeighborsIds();
         assert(a.isEmpty());
      }
 
-    public void testAddUpPointer() {
+    public void testAddUpPointer() 
+    {
        node0.addUpPointer(node1);
        HashSet<Integer> a = node0.getInvSurNeighborsIds();
        assert(!a.isEmpty());
     }
     
-    public void testRemoveUpPointer() {
+    public void testRemoveUpPointer() 
+    {
+        node0.addUpPointer(node1);
         node0.removeUpPointer(node1);
         HashSet<Integer> a = node0.getInvSurNeighborsIds();
         assert(a.isEmpty());
      }
     
-    public void testAddDownPointer() {
-        node0.addUpPointer(node1);
+    public void testAddDownPointer() 
+    {
+        node0.addDownPointer(node1);
         HashSet<Integer> a = node0.getSurNeighborsIds();
         assert(!a.isEmpty());
      }
      
-     public void testRemoveDownPointer() {
+     public void testRemoveDownPointer() 
+     {
          node0.removeUpPointer(node1);
          HashSet<Integer> a = node0.getSurNeighborsIds();
          assert(a.isEmpty());
-      }
+     }
     
     
     public void  testConstructSimplifiedNodeDomain() 
     {
+        SimplifiedNodeDomain snd = node0.constructSimplifiedNodeDomain();
+        ExpectedResult expectedResult = new ExpectedResult(1, 0);
+        assert snd.equals(expectedResult);
     
     }
     
@@ -74,6 +90,8 @@ public class Node2 extends TestCase
     
     public void testInsertSelf()
     {
+        node0.setFold(node0);
+        node0.setState(State.CAP);
         node1.insertSelf(node0);
         assert(node0.getHighestNeighbor().compareTo(node1) == 0);
         
@@ -88,11 +106,6 @@ public class Node2 extends TestCase
     
     public void testFindCapNode()
     {
-<<<<<<< OURS
-        
-=======
-        node1.insertSelf(node0);
-        
         for (int i = 2; i < 15; i++)
         { 
             Node node = new Node(i);
@@ -101,15 +114,14 @@ public class Node2 extends TestCase
         
         assert (node0.findCapNode(node0).getWebId() == 7);
     }
-    
+   
     public void testRemoveFromHyPeerWeb()
     {
-        node1.insertSelf(node0);
-        
->>>>>>> THEIRS
         for (int i = 2; i < 15; i++)
         { 
             Node node = new Node(i);
+            node.setFold(node0);
+            node.setState(State.CAP);
             node.insertSelf(node0);
         }
         
@@ -117,28 +129,13 @@ public class Node2 extends TestCase
         { 
             node0.removeFromHyPeerWeb();
         }
-<<<<<<< OURS
-        assert (node0.getHighestNeighbor().getWebId() == 1);
-    }
-   
-    public void testRemoveFromHyPeerWeb()
-=======
-        assert (node0.getHighestNeighbor().getWebId() == 1);
-    }
-    
-    public void testCompareTo()
->>>>>>> THEIRS
-    {
-<<<<<<< OURS
         
+        assert (node0.getHighestNeighbor().getWebId() == 1);
     }
      
     
     public void testGetLowestNeighborWithoutChild()
     {
         
-=======
-        assert(node0.compareTo(node0) == 0);    
->>>>>>> THEIRS
     }
 }
